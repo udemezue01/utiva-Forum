844d25156ec302e9d19863e57d6520db6ac3ff15
Udemezue John <johnzeus14@gmail.com>
2020-07-24 12:16:14 +0100

    added accounts

--- src/Utiva/Utiva/settings.py
+++ src/Utiva/Utiva/settings.py
@@ -124,4 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'account.User'
--- src/Utiva/account/views.py
+++ src/Utiva/account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+
+	return render(request, 'account/index.html')
+
+
+def login(request):
+	return render (request, 'account/login.html')
+
+def register(request):
+	return render (request, 'account/register.html')
